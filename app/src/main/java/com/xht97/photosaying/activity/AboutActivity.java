dd3731ef95092e5e8c1ab949b43ea05f4f1fec15
swx6868752 <swx6868752@163.com>
2018-04-16 22:28:37 +0800

    有个zz把n写成了m

--- app/src/main/java/com/xht97/photosaying/activity/AboutActivity.java
+++ app/src/main/java/com/xht97/photosaying/activity/AboutActivity.java
@@ -22,6 +22,7 @@ public class AboutActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+
                 this.finish();
         }
         return super.onOptionsItemSelected(item);
