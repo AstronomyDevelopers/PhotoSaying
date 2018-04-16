003b92edc0910a00e7c1fbe4c8d6ddf737a308d0
tommyxu97 <377935382@qq.com>
2018-04-16 15:27:31 +0800

    Release Beta Version 0.1
    包含模块框架演示

---
+++ app/src/main/java/com/xht97/photosaying/activity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.xht97.photosaying.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xht97.photosaying.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/xht97/photosaying/activity/MainActivity.java
+++ app/src/main/java/com/xht97/photosaying/activity/MainActivity.java
@@ -1,8 +1,15 @@
 package com.xht97.photosaying.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,11 +19,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.xht97.photosaying.R;
+import com.xht97.photosaying.adapter.ViewPagerAdapter;
+import com.xht97.photosaying.fragment.CommunityFragment;
+import com.xht97.photosaying.fragment.HomeFragment;
+import com.xht97.photosaying.fragment.MeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+    private MenuItem menuItem;
+    private View drawView;
+    // 记录下用户第一次点击的时间
+    private long firstTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +52,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, CreatePhotoActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -40,18 +63,80 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        // 菜单内导航视图监听
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        // NavigationView菜单头像点击事件监听
+        // 要实现这个功能，要使用动态加载头部布局文件，要不然findViewById会报NullPointer错误
+        drawView = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        ImageView imageView = (ImageView) drawView.findViewById(R.id.ivHeader);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+                boolean isUserLogin = sharedPreferences.getBoolean("isUserLogin", false);
+                if(!isUserLogin){
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // 设置底部导航栏监听器
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.button_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.button_community:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.buttom_me:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        // 给Adapter添加上首页的三个Fragment
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new CommunityFragment());
+        fragmentList.add(new MeFragment());
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragmentList);
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        // 设置页面翻动监听器
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null){
+                    menuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 
     @Override
@@ -61,6 +146,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // 右上角菜单选择监听
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -70,34 +156,67 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.action_about) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    // 监听抽屉式菜单和底部导航栏的点击事件
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if(id == R.id.nav_camera){
+            Intent intent = new Intent(MainActivity.this, CreatePhotoActivity.class);
+            startActivity(intent);
+        } else if(id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if(id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        } else if(id == R.id.nav_share) {
+            Intent textIntent = new Intent(Intent.ACTION_SEND);
+            textIntent.setType("text/plain");
+            textIntent.putExtra(Intent.EXTRA_TEXT, "欢迎使用图涵！由武汉大学的轩天文艺队开发。");
+            startActivity(Intent.createChooser(textIntent, "分享"));
+        } else if(id == R.id.nav_about){
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // 实现点击两下返回键退出程序
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+
+                long secondTime = System.currentTimeMillis();
+                if (secondTime-firstTime > 2000){
+                    Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                    firstTime = secondTime;
+                    return true;
+                } else {
+                    // 此处调用 System.exit(0); 也可实现退出，但是没有动画效果
+                    this.finish();
+                }
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
